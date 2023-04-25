dccf55a48a318cb2bec9114ddda5bdd9a0bd658f
121277 <121277@fhwn.ac.at>
2023-04-25 21:48:55 +0200

    added exe and lib projects

---
+++ verse-interpreter/Application.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace verse_interpreter.exe
+{
+    public class Application
+    {
+        public void Run()
+        {
+
+        }
+    }
+}
---
+++ verse-interpreter/Program.cs
@@ -0,0 +1,4 @@
+﻿using verse_interpreter.exe;
+
+Application app = new Application();
+app.Run();
