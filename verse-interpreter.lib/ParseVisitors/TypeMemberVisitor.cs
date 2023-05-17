7e3d4f354fe0054d4db2004f080124c0a813876d
MrNobdy-T <98278340+MrNobdy-T@users.noreply.github.com>
2023-05-17 21:42:09 +0200

    Added fixed variable update

--- verse-interpreter.lib/ParseVisitors/TypeMemberVisitor.cs
+++ verse-interpreter.lib/ParseVisitors/TypeMemberVisitor.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using verse_interpreter.lib.Data.DataVisitors;
 using verse_interpreter.lib.Data.ResultObjects;
 using verse_interpreter.lib.Grammar;
 
@@ -13,21 +14,23 @@ namespace verse_interpreter.lib.Visitors
     {
         private readonly ApplicationState _applicationState;
         private readonly ValueDefinitionVisitor _valueDefinitionVisitor;
+        private readonly VariableVisitor _variableVisitor;
 
         public TypeMemberVisitor(ApplicationState applicationState,
-                                 ValueDefinitionVisitor valueDefinitionVisitor) : base(applicationState)
+                                 ValueDefinitionVisitor valueDefinitionVisitor,
+                                 VariableVisitor variableVisitor) : base(applicationState)
         {
             _applicationState = applicationState;
             _valueDefinitionVisitor = valueDefinitionVisitor;
+            _variableVisitor = variableVisitor;
         }
 
         public override TypeMemberAccessResult VisitType_member_definition([NotNull] Verse.Type_member_definitionContext context)
         {
             var identfier = context.type_member_access().Accept(this);
-            var value = context.value_definition().Accept(_valueDefinitionVisitor);
-            //var instance = _applicationState.CurrentScope.LookupManager.GetInstanceVariable(identfier.VariableName);
-            throw new NotImplementedException();
-            //instance.LookupManager.UpdateVariable(identfier.PropertyName, value);
+            var value = Converter.VariableConverter.Convert(context.value_definition().Accept(_valueDefinitionVisitor));
+            var instance = _applicationState.CurrentScope.LookupManager.GetVariable(identfier.VariableName).AcceptDynamicType(_variableVisitor);
+            instance.LookupManager.UpdateVariable(value);
             return base.VisitType_member_definition(context);
         }
 
